016550b0c6453dca56fd9413a1ef1577b2570990
dks0398255 <julpanucci@gmail.com>
2017-03-28 20:30:55 -0400

    Converted MainController to act like

--- QNect/MainController.swift
+++ QNect/MainController.swift
@@ -11,8 +11,9 @@ import Pageboy
 import AVFoundation
 import TransitionTreasury
 import TransitionAnimation
+import PTPopupWebView
 
-class MainController: PageboyViewController,  NavgationTransitionable, ModalTransitionDelegate, UIGestureRecognizerDelegate  {
+class MainController: PageboyViewController, NavgationTransitionable, ModalTransitionDelegate  {
     
     /// Transiton delegate
     var tr_presentTransition: TRViewControllerTransitionDelegate?
@@ -21,39 +22,65 @@ class MainController: PageboyViewController,  NavgationTransitionable, ModalTran
  
     //MARK: Properties
     //todo: This controller will become scanner controller and scanner controller will be a place holder
-    var scannerViewController: ScannerViewController!
+    var placeHolderViewController: UIViewController!
     var profileViewController: UINavigationController!
     var connectionsViewController: UINavigationController!
     
-    
     var colorView: UIView!
     var toFromIndex: (Int, Int) = (0, 0)
+    let kDismissString = "Dismiss"
+    let kPinchVelocity = 8.0
+    var scannedContact = 0
+    var contactImage:UIImage?
+    var showURLAlert = 0
     
-    //todo: Handle this scanning stuff
     let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-    var captureSession:AVCaptureSession?
-    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var captureSession: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var message: String?
+    var contact: User!
+    var qrCodeFrameView = UIImageView()
+    var scannerCanScan = true
     
+    
+    //MARK: Actions
+    
+    @IBAction func gesture(_ sender: AnyObject) {
+        handleGesture(sender)
+    }
+    
+    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         colorView = UIView(frame: view.frame)
         view.insertSubview(colorView, at: 0)
         colorView.backgroundColor = .qnPurple
         colorView.alpha = 0.0
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        scannerViewController = storyboard.instantiateViewController(withIdentifier: "ScannerViewController") as! ScannerViewController
         profileViewController = storyboard.instantiateViewController(withIdentifier: "ProfileViewControllerNav") as! UINavigationController
         connectionsViewController = storyboard.instantiateViewController(withIdentifier: "ConnectionsViewControllerNav") as! UINavigationController
+        placeHolderViewController = storyboard.instantiateViewController(withIdentifier: "PlaceHolderViewController")
+        placeHolderViewController.view.alpha = 0.0
         
-
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(MainController.interactiveTransition(_:)))
+        pan.delegate = self
+        view.addGestureRecognizer(pan)
+        
+        createCaptureSession()
+        startCaptureSession()
+        
         self.dataSource = self
         self.delegate = self
-        
-        scannerViewController.view.alpha = 0.0
-        
+    }
+    
+    //MARK: Capture Session Functions
+    
+    func createCaptureSession()
+    {
         var error:NSError?
         let input:AnyObject!
         do {
@@ -68,20 +95,28 @@ class MainController: PageboyViewController,  NavgationTransitionable, ModalTran
         } else {
             captureSession = AVCaptureSession()
             captureSession?.addInput(input as! AVCaptureInput)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
             
             videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
             videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
             
             videoPreviewLayer?.frame = view.layer.bounds
-            view.layer.insertSublayer(videoPreviewLayer!, at: 0)
-            
-            captureSession?.startRunning()
+            view.layer.insertSublayer(videoPreviewLayer, at: 0)
         }
-        
-        
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(MainController.interactiveTransition(_:)))
-        pan.delegate = self
-        view.addGestureRecognizer(pan)
+    }
+    
+    func startCaptureSession()
+    {
+        captureSession?.startRunning()
+    }
+    
+    func stopCaptureSession()
+    {
+        captureSession?.stopRunning()
     }
     
     func interactiveTransition(_ sender: UIPanGestureRecognizer) {
@@ -107,7 +142,158 @@ class MainController: PageboyViewController,  NavgationTransitionable, ModalTran
   
 
 
+    //MARK: Scanning Contact
     
+    func handleScannedContact(_ metadataObj:AVMetadataMachineReadableCodeObject, barCodeObject:AVMetadataMachineReadableCodeObject)
+    {
+        self.stopCaptureSession()
+        segueToContactViewController()
+    }
+    
+    
+    
+    func centerForBarcodeObject(_ barCodeObject:AVMetadataMachineReadableCodeObject) -> CGPoint
+    {
+        let centerX = barCodeObject.bounds.origin.x + (barCodeObject.bounds.size.width / 2.0)
+        let centerY = barCodeObject.bounds.origin.y + (barCodeObject.bounds.size.height / 2.0)
+        let center = CGPoint(x: centerX, y: centerY)
+        return center
+    }
+    
+    //Pinch to zoom
+    func handleGesture(_ sender: AnyObject)
+    {
+        let pinchVelocityDividerFactor = kPinchVelocity;
+        
+        
+        if (sender.state == UIGestureRecognizerState.changed) {
+            let pinch = sender as! UIPinchGestureRecognizer
+            do {
+                try captureDevice?.lockForConfiguration()
+                
+                let desiredZoomFactor = Double((captureDevice?.videoZoomFactor)!) + atan2(Double(pinch.velocity), pinchVelocityDividerFactor)
+                
+                captureDevice?.videoZoomFactor = max(1.0,min(CGFloat(desiredZoomFactor), (captureDevice?.activeFormat.videoMaxZoomFactor)!))
+                
+                captureDevice?.unlockForConfiguration()
+            }catch let error as NSError {
+                print(error)
+            }
+        }
+    }
+    
+    
+    
+    //MARK: Segue
+    
+    func segueToContactViewController()
+    {
+        performSegue(withIdentifier: SegueIdentifiers.Contact, sender: self)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == SegueIdentifiers.Contact {
+            if let contactVC = (segue.destination as! UINavigationController).topViewController as? ContactViewController {
+                contactVC.configureViewController(self.contact!)
+            }
+        }
+    }
+    
+
+}
+
+extension MainController: AVCaptureMetadataOutputObjectsDelegate {
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        
+        guard scannerCanScan else {
+            //If scannerCanScan is set to true we must be on a different page where we will not allow scanning
+            return
+        }
+        
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            showURLAlert = 0
+            return
+        } else {
+            
+            let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            
+            if metadataObj.type == AVMetadataObjectTypeQRCode{
+                if let contact = QnDecoder.decodeQRCode(metadataObj.stringValue) {
+                    
+                    self.contact = contact
+                    
+                    handleScannedContact(metadataObj, barCodeObject: barCodeObject)
+                    
+                }else if metadataObj.stringValue.contains(".com") {
+                    
+                    //Todo: Need to test different QRCodes and handle different strings
+                    var url = ""
+                    if !metadataObj.stringValue.contains("http"){
+                        url = "http://\(metadataObj.stringValue)"
+                    }else {url = metadataObj.stringValue}
+                    
+                    
+                    let popupvc = PTPopupWebViewController()
+                    popupvc.popupView.URL(string: url)
+                    let closeButton = PTPopupWebViewButton(type: .custom).title("Close").foregroundColor(UIColor.qnBlue)
+                    closeButton.handler({
+                        self.startCaptureSession()
+                        popupvc.close()
+                    })
+                    
+                    let safariButton = PTPopupWebViewButton(type: .custom).backgroundColor(UIColor.qnBlue).foregroundColor(UIColor.white)
+                    safariButton.title("Open in Safari")
+                    safariButton.handler({
+                        UIApplication.shared.openURL(URL(string: url)!)
+                    })
+                    
+                    popupvc.popupView.addButton(safariButton)
+                    popupvc.popupView.addButton(closeButton)
+                    popupvc.show()
+                    self.stopCaptureSession()
+                }
+                else {
+                    let alert = UIAlertController(title: nil, message: metadataObj.stringValue, preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: kDismissString, style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
+
+extension MainController: PageboyViewControllerDelegate {
+    // MARK: PageboyViewControllerDelegate
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController,
+                               willScrollToPageAtIndex index: Int,
+                               direction: PageboyViewController.NavigationDirection,
+                               animated: Bool) {
+        //If we are scrolling then disable scanning
+        scannerCanScan = false
+        
+        toFromIndex = calculateToFromIndexTuple(direction: direction, index: index)
+    }
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController,
+                               didScrollToPosition position: CGPoint,
+                               direction: PageboyViewController.NavigationDirection,
+                               animated: Bool) {
+        updateColorViewAlpha(position: position)
+    }
+    
+    func pageboyViewController(_ pageboyViewController: PageboyViewController,
+                               didScrollToPageAtIndex index: Int,
+                               direction: PageboyViewController.NavigationDirection,
+                               animated: Bool) {
+        
+        //If not on the scanner page then we do not allow scanning
+        scannerCanScan = index != 1 ? false : true
+        
+    }
     
     func calculateToFromIndexTuple(direction: PageboyViewController.NavigationDirection, index: Int) -> (Int, Int)
     {
@@ -148,38 +334,12 @@ class MainController: PageboyViewController,  NavgationTransitionable, ModalTran
 
 }
 
-extension MainController: PageboyViewControllerDelegate {
-    // MARK: PageboyViewControllerDelegate
-    
-    func pageboyViewController(_ pageboyViewController: PageboyViewController,
-                               willScrollToPageAtIndex index: Int,
-                               direction: PageboyViewController.NavigationDirection,
-                               animated: Bool) {
-        
-        toFromIndex = calculateToFromIndexTuple(direction: direction, index: index)
-    }
-    
-    func pageboyViewController(_ pageboyViewController: PageboyViewController,
-                               didScrollToPosition position: CGPoint,
-                               direction: PageboyViewController.NavigationDirection,
-                               animated: Bool) {
-        updateColorViewAlpha(position: position)
-    }
-    
-    func pageboyViewController(_ pageboyViewController: PageboyViewController,
-                               didScrollToPageAtIndex index: Int,
-                               direction: PageboyViewController.NavigationDirection,
-                               animated: Bool) {
-        
-    }
-}
-
 extension MainController: PageboyViewControllerDataSource {
     //MARK: Pageboy Datasource
     
     func viewControllers(forPageboyViewController pageboyViewController: PageboyViewController) -> [UIViewController]? {
         // return array of view controllers
-        return [profileViewController, scannerViewController, connectionsViewController]
+        return [profileViewController, placeHolderViewController, connectionsViewController]
     }
     
     func defaultPageIndex(forPageboyViewController pageboyViewController: PageboyViewController) -> PageboyViewController.PageIndex? {
@@ -188,3 +348,16 @@ extension MainController: PageboyViewControllerDataSource {
     }
     
 }
+
+
+extension MainController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let ges = gestureRecognizer as? UIPanGestureRecognizer {
+            return ges.translation(in: ges.view).y != 0
+        }
+        return false
+    }
+}
+
+
+
