dfa0f8cc77a3145335bd1c859feb8a08dcb4d7a0
Julian Panucci <julpanucci@gmail.com>
2017-05-14 11:13:16 -0400

    Created Alias file

---
+++ QNect/Alias.swift
@@ -0,0 +1,13 @@
+//
+//  Alias.swift
+//  QNect
+//
+//  Created by Panucci, Julian R on 5/14/17.
+//  Copyright © 2017 Julian Panucci. All rights reserved.
+//
+
+import Foundation
+
+
+typealias ErrorCompletion = (Error?) -> Void
+
