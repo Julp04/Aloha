0b7c25b609bcb6ec51822856ec0bd311defa3448
Julian Panucci <julpanucci@gmail.com>
2017-05-24 15:52:42 -0400

    Added FollowRequestsController

---
+++ Aloha/FollowRequestsViewController.swift
@@ -0,0 +1,184 @@
+//
+//  FollowRequestsViewController.swift
+//  Aloha
+//
+//  Created by Panucci, Julian R on 5/24/17.
+//  Copyright © 2017 Julian Panucci. All rights reserved.
+//
+
+import UIKit
+
+class FollowRequestsViewController: UITableViewController {
+    
+    //MARK: Constants
+    
+    //MARK: Properties
+    
+    var followRequests: [User]!
+    
+    //MARK: Outlets
+    
+    //MARK: Actions
+    
+    //MARK: Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        tableView.allowsSelection = false
+        
+    }
+    
+    
+    func configureViewController(requests: [User]) {
+        self.followRequests = requests
+    }
+
+   
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return followRequests.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowRequestCell") as! FollowRequestCell
+        
+        let user = followRequests[indexPath.row]
+        let name = user.fullName()
+        let username = user.username
+        
+        cell.nameLabel.text = name
+        cell.usernameLabel.text = username
+        
+        cell.acceptButton.tag = indexPath.row
+        cell.declineButton.tag = indexPath.row
+        cell.statusButton.tag = indexPath.row
+        
+        cell.profileImageView.image = user.profileImage
+        
+        cell.acceptButton.addTarget(self, action: #selector(FollowRequestsViewController.acceptRequest(sender:)), for: .touchUpInside)
+        cell.declineButton.addTarget(self, action: #selector(FollowRequestsViewController.declineRequest(sender:)), for: .touchUpInside)
+        
+        cell.statusButton.isHidden = true
+     
+        
+        
+       
+        
+        return cell
+        
+    }
+    
+    
+    func updateStatusButton(sender: UIButton) {
+        let index = sender.tag
+        let user = followRequests[index]
+        
+        let indexPath = IndexPath(row: index, section: 0)
+        let cell = tableView.cellForRow(at: indexPath) as! FollowRequestCell
+        
+        //This might have to be observed only once
+        QnClient.sharedInstance.getFollowStatus(user: user) { (status) in
+            cell.statusButton.removeTarget(nil, action: nil, for: .allEvents)
+            switch status {
+            case .accepted:
+                cell.statusButton.isHidden = false
+                cell.statusButton.setTitle("Following", for: .normal)
+                cell.statusButton.backgroundColor = UIColor.qnBlue
+                cell.statusButton.setTitleColor(.white, for: .normal)
+                cell.statusButton.addTarget(self, action: #selector(FollowRequestsViewController.showUnfollowAlert(sender:)), for: .touchUpInside)
+            case .notFollowing:
+                cell.statusButton.isHidden = false
+                cell.statusButton.setTitle("Follow", for: .normal)
+                cell.statusButton.backgroundColor = UIColor.white
+                cell.statusButton.setTitleColor(.qnBlue, for: .normal)
+                cell.statusButton.addTarget(self, action: #selector(FollowRequestsViewController.followUser(sender:)), for: .touchUpInside)
+            case .pending:
+                cell.statusButton.isHidden = false
+                cell.statusButton.setTitle("Pending", for: .normal)
+                cell.statusButton.backgroundColor = UIColor.qnBlue
+                cell.statusButton.setTitleColor(.white, for: .normal)
+                cell.statusButton.addTarget(self, action: #selector(FollowRequestsViewController.showCancelRequestAlert(sender:)), for: .touchUpInside)
+                
+            default:
+                cell.statusButton.isHidden = true
+                cell.acceptButton.isHidden = true
+                cell.declineButton.isHidden = true
+            }
+        }
+    }
+    
+    func acceptRequest(sender: UIButton) {
+      
+        let index = sender.tag
+        let user = followRequests[index]
+        QnClient.sharedInstance.acceptFollowRequest(user: user)
+//        updateStatusButton(sender: sender)
+
+    }
+    
+    func declineRequest(sender: UIButton) {
+        let index = sender.tag
+        let user = followRequests[index]
+        QnClient.sharedInstance.denyFollowRequest(user: user)
+//        updateStatusButton(sender: sender)
+    }
+    
+    func followUser(sender: UIButton) {
+        let index = sender.tag
+        
+        let user = followRequests[index]
+        
+        QnClient.sharedInstance.getUpdatedInfoForUser(user: user) { (user) in
+            QnClient.sharedInstance.follow(user: user) { (error) in
+                if error != nil {
+                    print(error!)
+                }
+            }
+        }
+        
+        
+    }
+    
+    func showUnfollowAlert(sender: UIButton) {
+        let index = sender.tag
+        let user = followRequests[index]
+        
+        let alert = UIAlertController(title: user.username, message: nil, preferredStyle: .actionSheet)
+        let unfollowAction = UIAlertAction(title: "Unfollow", style: .destructive) { (action) in
+            QnClient.sharedInstance.unfollow(user: user)
+        }
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        alert.addAction(unfollowAction)
+        alert.addAction(dismissAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showCancelRequestAlert(sender: UIButton) {
+        let index = sender.tag
+        let user = followRequests[index]
+        
+        let alert = UIAlertController(title: user.username, message: nil, preferredStyle: .actionSheet)
+        let cancelRequestAction = UIAlertAction(title: "Cancel follow request", style: .destructive) { (action) in
+            QnClient.sharedInstance.cancelFollow(user: user)
+        }
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        alert.addAction(cancelRequestAction)
+        alert.addAction(dismissAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+  
+
+}
