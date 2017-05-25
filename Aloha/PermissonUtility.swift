6835d2ea6967f3c38a2600cf8652c24897a7b826
Julian Panucci <julpanucci@gmail.com>
2017-05-25 16:20:27 -0400

    Added PermissionUtility to check for camera permissions

---
+++ Aloha/PermissonUtility.swift
@@ -0,0 +1,35 @@
+//
+//  PermissonUtility.swift
+//  Aloha
+//
+//  Created by Panucci, Julian R on 5/25/17.
+//  Copyright © 2017 Julian Panucci. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+class PermissonUtility {
+    
+    
+    
+    
+    static func isCameraAuthorized(completion: @escaping (Bool) -> Void) {
+        
+        if !Platform.isSimulator {
+            let cameraMediaType = AVMediaTypeVideo
+            let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(forMediaType: cameraMediaType)
+            
+            switch cameraAuthorizationStatus {
+            case .authorized: completion(true)
+            case .notDetermined, .denied, .restricted:
+                AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { (success) in
+                    completion(success)
+                })
+            }
+        }
+    }
+    
+
+}
