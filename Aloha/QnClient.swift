79c4a99c708dda2fc3ca7a91f0093ee5cd1989f6
Julian Panucci <julpanucci@gmail.com>
2017-09-05 14:58:22 -0400

    Fixed typo adding twitter user

--- Aloha/QnClient.swift
+++ Aloha/QnClient.swift
@@ -763,7 +763,7 @@ class QnClient {
         
         let currentUser = FIRAuth.auth()!.currentUser!
         
-        ref.child("users").child(currentUser.uid).child("accounts").child("twitter").updateChildValues(["screenName":screenName, "token:":token, "tokenSecret": tokenSecret])
+        ref.child("users").child(currentUser.uid).child("accounts").child("twitter").updateChildValues(["screenName":screenName, "token":token, "tokenSecret": tokenSecret])
         
         ref.child("accounts").child("twitter").updateChildValues([screenName: "screenName"])
     }
